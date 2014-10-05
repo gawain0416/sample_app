e66315003076495069045de4f4c8085c6f639954
gawain0416 <gawain@localhost.localdomain>
2014-10-05 21:38:46 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
 
 root to: 'static_pages#home'
 
   #get 'static_pages/help'
   #get "static_pages/about"
   #get "static_pages/contact"
+  resources :users
+
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
